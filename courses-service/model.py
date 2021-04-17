427cb1cd2aa9e27f68c712a78beaa5cc15f83835
Cristina Silva <crissilva.eng@gmail.com>
2021-04-17 01:06:52 +0000

    Moved something to somewhere... goodnight...

--- courses-service/model.py
+++
@@ -1,6 +0,0 @@
-from config import db
-
-
-class Course(db.Model):
-    code = db.Column(db.String(15), primary_key=True, unique=True)
-    name = db.Column(db.String(127), nullable=False)
--- student-service/model.py
+++
@@ -1,6 +0,0 @@
-from config import db
-
-
-class Student(db.Model):
-    email = db.Column(db.String(127), primary_key=True, unique=True)
-    name = db.Column(db.String(127), nullable=False)
